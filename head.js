5de8f360921464abb7b99c3b97b2f8c5b0ad3429
tatianibassani <tatianibassani@gmail.com>
2022-10-17 21:37:14 +0000

    improving assertEqual and create a function head

--- head.js
+++ head.js
@@ -5,5 +5,13 @@ const assertEqual = function(actual, expected) {
     console.log(`🛑🛑🛑 Assertion Failed: ${actual}  !== ${expected}`);
   }
 };
+
+const head = function(values){
+  return values[0];
+}
+
 assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(1, 1);
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
