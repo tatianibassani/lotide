f21d030605d4fad26605f077c25ae2eea3dc1241
tatianibassani <tatianibassani@gmail.com>
2022-10-24 22:33:34 +0000

    converting tailTest into mocha/chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,10 +1,10 @@
-const assertEqual = require('../assertEqual');
-const tail = require('../tail')
+const assert = require('chai').assert;
+const tail   = require('../tail');
 
-
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-const result = tail(words);
-assertEqual(result.toString(), ["Lighthouse", "Labs"].toString());
+describe("#tail", () => {
+  it('returns ["Lighthouse", "Labs"] for ["Yo Yo", "Lighthouse", "Labs"]', () => {
+    const words = ["Yo Yo", "Lighthouse", "Labs"];
+    const result = tail(words);
+    assert.strictEqual(result.toString(), ["Lighthouse", "Labs"].toString());
+  });
+});
