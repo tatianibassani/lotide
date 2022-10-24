ddb5f9899f6862d6a35b6cfb47b012b50845d028
tatianibassani <tatianibassani@gmail.com>
2022-10-24 22:20:08 +0000

    converting headTest into mocha/chai

--- test/assertArraysEqualTest.js
+++ test/assertArraysEqualTest.js
@@ -2,5 +2,5 @@
 const eqArrays = require('../assertEqual');
 const assertArraysEqual = require('../assertArraysEqual');
 
-assertArraysEqual(eqArrays([1,2,3], [4,5,6]), false);
-assertArraysEqual(eqArrays([1,2,3], [1,2,3]), true);
+assertArraysEqual([1,2,3], [4,5,6], false);
+assertArraysEqual([1,2,3], [1,2,3], true);
--- test/headTest.js
+++ test/headTest.js
@@ -1,8 +1,13 @@
-const assertEqual = require('../assertEqual');
-const head = require('../head');
+const assert = require('chai').assert;
+const head   = require('../head');
 
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+describe("#head", () => {
+  it("returns 5 for [5,6,7]", () => {
+    assert.strictEqual(head([5,6,7]), 5);
+  });
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+  it("returns 'Hello' for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), 'Hello'); 
+  });
+});
+
