549f4d027608b1b23008a9b1ffcefb50aae5b8e0
tatianibassani <tatianibassani@gmail.com>
2023-11-17 23:53:57 +0000

    add new test

--- without.js
+++ without.js
@@ -28,3 +28,6 @@ const without = function(source, itemsToRemove){
 
 assertArraysEqual(without([1, 2, 3], [1]), [2, 3]);
 assertArraysEqual(without(["1", "2", "3"], [1, 2, "3"]), ["1", "2"]);
+const words = ["hello", "world", "lighthouse"];
+without(words, ["lighthouse"]); 
+assertArraysEqual(words, ["hello", "world", "lighthouse"]); // => should PASS
