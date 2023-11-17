b062486005a6047131944f87ea4a4ef66c14f2b8
tatianibassani <tatianibassani@gmail.com>
2023-11-17 17:54:50 +0000

    implementing assertArrayEqual function

---
+++ assertArraysEqual.js
@@ -0,0 +1,15 @@
+const eqArrays = function (arr1, arr2){
+  if (arr1.toString() === arr2.toString()) {
+    return true;
+  }
+  return false;
+  
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅✅✅ Assertion Passed: ${arr1}  === ${arr2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${arr1}  !== ${arr2}`);
+  }
+};
