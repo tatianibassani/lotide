e6633a80b64b96458db4a87a27aae1dc5d911706
tatianibassani <tatianibassani@gmail.com>
2023-11-17 23:50:20 +0000

    implementing without function

---
+++ without.js
@@ -0,0 +1,30 @@
+const eqArrays = function (arr1, arr2){
+  if (arr1.toString() === arr2.toString()) {
+    return true;
+  }
+  return false;
+  
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅✅✅ Assertion Passed: ${arr1}  === ${arr2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${arr1}  !== ${arr2}`);
+  }
+};
+
+const without = function(source, itemsToRemove){
+  let result = [];
+    
+  for (let i = 0; i < source.length; i++) {
+    if (!itemsToRemove.includes(source[i])) {
+      result.push(source[i]);
+    }
+  }
+
+  return result;
+}
+
+assertArraysEqual(without([1, 2, 3], [1]), [2, 3]);
+assertArraysEqual(without(["1", "2", "3"], [1, 2, "3"]), ["1", "2"]);
