61c209490ea956aeea0dd4d382d6e6eeda1e5d13
tatianibassani <tatianibassani@gmail.com>
2023-11-23 21:12:02 +0000

    impr. eqObjects

--- eqObjects.js
+++ eqObjects.js
@@ -7,18 +7,32 @@ const assertEqual = function(actual, expected) {
   }
 };
 
+function eqArrays(arr1, arr2) {
+  
+  //if (arr1.length !== arr2.length) {
+    //return false;
+  //}
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false; 
+    }
+  }
+
+  return true; 
+}
+
 const eqObjects = function(object1, object2){
   if (Object.keys(object1).length != Object.keys(object2).length) {
     return false;
   }
 
   for (const key of Object.keys(object1)) {
-    if (Array.isArray(object1[key])) {
+    if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
       if (!eqArrays(object1[key], object2[key])) {
          return false; 
       }
     } else {
-      if (!object1[key] === object2[key]) {
+      if (object1[key] !== object2[key]) {
         return false;
       }
     }
@@ -47,3 +61,6 @@ assertEqual(eqObjects(shirtObject , anotherShirtObject), true); // => true
 
 const longSleeveShirtObject= { size: "medium", color: "red", sleeveLength: "long" };
 assertEqual(eqObjects(shirtObject , longSleeveShirtObject), false); // => false
+
+const containsArrayObject= { size: "medium", color: "red", sleeveLength: "long", options: ["small", "medium"] };
+assertEqual(eqObjects(shirtObject , containsArrayObject), false); // => false
