a2e241728bded80f62a4de0192a06d2e4a32eb7c
tatianibassani <tatianibassani@gmail.com>
2023-11-23 21:43:28 +0000

    small adjustments

--- eqObjects.js
+++ eqObjects.js
@@ -9,9 +9,9 @@ const assertEqual = function(actual, expected) {
 
 function eqArrays(arr1, arr2) {
   
-  //if (arr1.length !== arr2.length) {
-    //return false;
-  //}
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
   for (let i = 0; i < arr1.length; i++) {
     if (arr1[i] !== arr2[i]) {
       return false; 
@@ -22,7 +22,7 @@ function eqArrays(arr1, arr2) {
 }
 
 const eqObjects = function(object1, object2){
-  if (Object.keys(object1).length != Object.keys(object2).length) {
+  if (Object.keys(object1).length !== Object.keys(object2).length) {
     return false;
   }
 
@@ -40,20 +40,6 @@ const eqObjects = function(object1, object2){
 
   return true;
 };
-
-// const eqObjects = function(object1, object2) {
-//   for (const key in object1) {
-//     for (const key in object2) {
-//       if (object1[key] !== object2[key]) {
-//         return false;
-//       }
-//     }
-//   }
-
-  
-
-//   return true;
-// };
 
 const shirtObject = { color: "red", size: "medium" };
 const anotherShirtObject= { size: "medium", color: "red" };
