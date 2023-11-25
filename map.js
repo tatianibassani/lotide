98af9af5aaa311e9c16250838aba2441464435d4
tatianibassani <tatianibassani@gmail.com>
2023-11-25 16:09:20 +0000

    implementing map function

---
+++ map.js
@@ -0,0 +1,35 @@
+function eqArrays(arr1, arr2) {
+  
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false; // Return false if elements at the same index are not equal
+    }
+  }
+
+  return true; 
+}
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅✅✅ Assertion Passed: ${arr1}  === ${arr2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${arr1}  !== ${arr2}`);
+  }
+};
+
+const words = ["ground", "control", "to", "major", "tom"];
+
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+}
+
+const results1 = map(words, word => word[0]);
+
+assertArraysEqual(results1, [ 'g', 'c', 't', 'm', 't' ]);  
