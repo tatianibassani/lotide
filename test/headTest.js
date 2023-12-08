13db604d146d768dee59ddb6da483c1507507139
Tatiana <tatianibassani@gmail.com>
2023-12-08 22:52:17 +0000

    converting test to mocha

--- test/headTest.js
+++ test/headTest.js
@@ -1,7 +1,19 @@
+const assert = require('chai').assert;
+const head   = require('../head');
 
-const head = require('../head');
-const assertEqual = require('../assertEqual');
+describe("#head", () => {
+  it("returns 5 for [5,6,7]", () => {
+    assert.strictEqual(head([5,6,7]), 5);
+  });
+
+  it("returns 'Hello' for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), 'Hello'); 
+  });
+});
+
+// const head = require('../head');
+// const assertEqual = require('../assertEqual');
  
- assertEqual(head([5,6,7]), 5);
- assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
- assertEqual(head([1,3,4]), 5);
+//  assertEqual(head([5,6,7]), 5);
+//  assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+//  assertEqual(head([1,3,4]), 5);
