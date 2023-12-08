3f6f1404123201db9b76bf3d39925353118910fc
Tatiana <tatianibassani@gmail.com>
2023-12-08 21:25:55 +0000

    returning a empty array

--- tail.js
+++ tail.js
@@ -1,11 +1,11 @@
-const assertEqual = require('./assertEqual');
+const assertEqual = require('../assertEqual');
 
 const tail = function(values) {
     
   if (values.length > 1) {
     return values.slice(1);
   } else {
-    return values;
+    return [];
   }
 };
 
