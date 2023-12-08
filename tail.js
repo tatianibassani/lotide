92830b5a734f0cf5cb884dc961fe4ba5e0e39300
Tatiana <tatianibassani@gmail.com>
2023-12-08 21:21:06 +0000

    refactoring tail and implementing tail test

--- tail.js
+++ tail.js
@@ -1,12 +1,4 @@
-const assertEqual = function(actual, expected) {
-
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual}  === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual}  !== ${expected}`);
-  }
-};
-
+const assertEqual = require('./assertEqual');
 
 const tail = function(values) {
     
@@ -16,5 +8,5 @@ const tail = function(values) {
     return values;
   }
 };
-assertEqual(tail([5,6,7]).toString(), [6,7].toString());
-assertEqual(tail([1,2,5,8,0,8]).toString(), [2,5,8,0,4].toString());
+
+module.exports = tail;
