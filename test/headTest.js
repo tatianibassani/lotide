50386822836d451ad41dedb873dfac7b085c6b9f
Tatiana <tatianibassani@gmail.com>
2023-12-08 21:28:11 +0000

    require assertEqual

--- test/headTest.js
+++ test/headTest.js
@@ -1,3 +1,4 @@
+
 const head = require('./head');
  
  assertEqual(head([5,6,7]), 5);
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,4 +1,4 @@
-
+const assertEqual = require('../assertEqual');
 const tail   = require('../tail');
 
 assertEqual(tail([5,6,7]).toString(), [6,7].toString());
