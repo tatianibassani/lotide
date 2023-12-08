e18c3ed8d9613c48c5c2a79036b0dcbe98a37387
Tatiana <tatianibassani@gmail.com>
2023-12-08 21:55:17 +0000

    improv

--- test/assertArraysEqualTest.js
+++ test/assertArraysEqualTest.js
@@ -1,4 +1,4 @@
 const assertArraysEqual = require('../assertArraysEqual');
 
-assertArraysEqual([1,2,3], [4,5,6], false);
-assertArraysEqual([1,2,3], [1,2,3], true);
+assertArraysEqual([1,2,3], [4,5,6]);
+assertArraysEqual([1,2,3], [1,2,3]);
