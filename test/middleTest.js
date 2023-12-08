f49488cd5a3244443fed05eb4d69e5992f1ab47b
Tatiana <tatianibassani@gmail.com>
2023-12-08 22:06:12 +0000

    imrpov

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,4 +1,5 @@
-const middle = require('../midle');
+const assertArraysEqual = require('../assertArraysEqual');
+const middle = require('../middle');
 
 assertArraysEqual(middle([7, 3, 4, 6, 2, 1]), [4, 6]);
 assertArraysEqual(middle([7, 3, 4, 6, 2]), [4]);
